b4443796a7d39d917f969ba56074b64193fb1709
AmirHossein <amirh.kargar85@gmail.com>
2024-07-20 20:07:40 +0330

    update: add --host and --port to serve and start:apiserver commands

--- src/Foundation/Server/APIServeCommand.php
+++ src/Foundation/Server/APIServeCommand.php
@@ -14,10 +14,23 @@ class APIServeCommand extends Command
     {
         if ($this->getOption('h') == 'h') $this->output->message($this->description, true);
 
-        $BOT_API_SERVER_DIR = $_ENV['BOT_API_SERVER_DIR'] == '' ? app('path.storage') . '/API-Server' : $_ENV['BOT_API_SERVER_DIR'];
-        if (!file_exists($BOT_API_SERVER_DIR)) mkdir($BOT_API_SERVER_DIR, recursive: true);
         if ($_ENV['API_ID'] == null || $_ENV['API_HASH'] == null) $this->output->failed("API_ID or API_HASH not set!", exit: true);
-        $this->output->success("Starting API Server on 127.0.0.1:8081");
-        exec("telegram-bot-api --local --api-id={$_ENV['API_ID']} --api-hash={$_ENV['API_HASH']} --dir={$BOT_API_SERVER_DIR}");
+        if (($_ENV['BOT_API_SERVER_IP'] == null && $this->options['host'] == null) || ($_ENV['BOT_API_SERVER_PORT'] == null && $this->options['port'] == null)) $this->output->failed("BOT_API_SERVER_IP or BOT_API_SERVER_PORT not set!", exit: true);
+
+        $port = $this->options['port'] ?? $_ENV['BOT_API_SERVER_PORT'];
+        $host = $this->options['host'] ?? $_ENV['BOT_API_SERVER_IP'];
+        $dir = $_ENV['BOT_API_SERVER_DIR'] ?? app('path.storage') . '/API-Server';
+
+        if (!file_exists($dir)) mkdir($dir, recursive: true);
+
+        $this->output->success("Starting API Server on {$host}:{$port}");
+
+        $command = "telegram-bot-api --local --http-ip-address={$host} --http-port={$port} --api-id={$_ENV['API_ID']} --api-hash={$_ENV['API_HASH']} --dir={$dir}";
+
+        if ($_ENV['BOT_API_SERVER_LOG_DIR'] != '') $command .= " --log={$_ENV['BOT_API_SERVER_LOG_DIR']}";
+        if ($_ENV['BOT_API_SERVER_STAT_IP'] != '') $command .= " --http-stat-ip-address={$_ENV['BOT_API_SERVER_STAT_IP']}";
+        if ($_ENV['BOT_API_SERVER_STAT_PORT'] != '') $command .= " --http-stat-port={$_ENV['BOT_API_SERVER_STAT_PORT']}";
+
+        exec($command);
     }
 }
--- src/Foundation/Server/ServeCommand.php
+++ src/Foundation/Server/ServeCommand.php
@@ -14,15 +14,32 @@ class ServeCommand extends Command
     {
         if ($this->getOption('h') == 'h') $this->output->message($this->description, true);
 
-        $DEVELOPMENT_SERVER_IP = $_ENV['DEVELOPMENT_SERVER_IP'] == '' ? '127.0.0.1' : $_ENV['DEVELOPMENT_SERVER_IP'];
-        $DEVELOPMENT_SERVER_PORT = $_ENV['DEVELOPMENT_SERVER_PORT'] == '' ? '9000' : $_ENV['DEVELOPMENT_SERVER_PORT'];
+        if (($_ENV['DEVELOPMENT_SERVER_IP'] == null && $this->options['host'] == null) || ($_ENV['DEVELOPMENT_SERVER_PORT'] == null && $this->options['port'] == null)) $this->output->failed("DEVELOPMENT_SERVER_IP or DEVELOPMENT_SERVER_PORT not set!", exit: true);
 
-        if ($this->options == null) exec("php -S {$DEVELOPMENT_SERVER_IP}:{$DEVELOPMENT_SERVER_PORT}");
+        $DEVELOPMENT_SERVER_PORT = $this->options['port'] ?? $_ENV['DEVELOPMENT_SERVER_PORT'];
+        $DEVELOPMENT_SERVER_IP = $this->options['host'] ?? $_ENV['DEVELOPMENT_SERVER_IP'];
+
         if ($this->getOption('apiserver') == 'apiserver') {
-            $BOT_API_SERVER_DIR = $_ENV['BOT_API_SERVER_DIR'] == '' ? app('path.storage') . '/API-Server' : $_ENV['BOT_API_SERVER_DIR'];
-            if (!file_exists($BOT_API_SERVER_DIR)) mkdir($BOT_API_SERVER_DIR, recursive: true);
             if ($_ENV['API_ID'] == null || $_ENV['API_HASH'] == null) $this->output->failed("API_ID or API_HASH not set!", exit: true);
-            exec("telegram-bot-api --local --api-id={$_ENV['API_ID']} --api-hash={$_ENV['API_HASH']} --dir={$BOT_API_SERVER_DIR} & php -S {$DEVELOPMENT_SERVER_IP}:{$DEVELOPMENT_SERVER_PORT}");
+            if ($_ENV['BOT_API_SERVER_IP'] == null || $_ENV['BOT_API_SERVER_PORT'] == null) $this->output->failed("BOT_API_SERVER_IP or BOT_API_SERVER_PORT not set!", exit: true);
+
+            $dir = $_ENV['BOT_API_SERVER_DIR'] ?? app('path.storage') . '/API-Server';
+
+            if (!file_exists($dir)) mkdir($dir, recursive: true);
+
+            $this->output->success("Starting API Server on {$_ENV['BOT_API_SERVER_IP']}:{$_ENV['BOT_API_SERVER_PORT']}");
+
+            $command = "telegram-bot-api --local --http-ip-address={$_ENV['BOT_API_SERVER_IP']} --http-port={$_ENV['BOT_API_SERVER_PORT']} --api-id={$_ENV['API_ID']} --api-hash={$_ENV['API_HASH']} --dir={$dir}";
+
+            if ($_ENV['BOT_API_SERVER_LOG_DIR'] != '') $command .= " --log={$_ENV['BOT_API_SERVER_LOG_DIR']}";
+            if ($_ENV['BOT_API_SERVER_STAT_IP'] != '') $command .= " --http-stat-ip-address={$_ENV['BOT_API_SERVER_STAT_IP']}";
+            if ($_ENV['BOT_API_SERVER_STAT_PORT'] != '') $command .= " --http-stat-port={$_ENV['BOT_API_SERVER_STAT_PORT']}";
+
+            $command .= " & php -S {$DEVELOPMENT_SERVER_IP}:{$DEVELOPMENT_SERVER_PORT}";
+
+            exec($command);
+        } else{
+            exec("php -S {$DEVELOPMENT_SERVER_IP}:{$DEVELOPMENT_SERVER_PORT}");
         }
     }
 }
