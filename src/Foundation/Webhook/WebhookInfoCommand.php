58dac9c75c26734888cece859cf064a50ac8dc5a
AmirHossein <amirh.kargar85@gmail.com>
2024-08-30 23:54:02 +0330

    fix: fix webhook:info drawer and listener type hint

--- src/Foundation/Webhook/WebhookInfoCommand.php
+++ src/Foundation/Webhook/WebhookInfoCommand.php
@@ -4,14 +4,13 @@ namespace LaraGram\Foundation\Webhook;
 
 use LaraGram\Console\Command;
 use LaraGram\Support\Facades\Console;
-use LaraGram\Support\Facades\Request;
 
 class WebhookInfoCommand extends Command
 {
     protected $signature = 'webhook:info';
     protected $description = 'Get webhook information';
 
-    public function handle()
+    public function handle(): void
     {
         if ($this->getOption('h') == 'h') Console::output()->message($this->description, true);
 
@@ -22,24 +21,24 @@ class WebhookInfoCommand extends Command
         }
 
         $url = "URL: " . str_replace('https://', '', str_replace('http://', '', $info['url']));
-        $len = strlen($url);
 
         $pending_update = "Pending Updates: {$info['pending_update_count']}";
-        $pending_update .= str_repeat(' ', $len - strlen($pending_update));
-
         $ip_address = "IP Address: {$info['ip_address']}";
-        $ip_address .= str_repeat(' ', $len - strlen($ip_address));
-
-        $certificate = "Certificate: {$info['has_custom_certificate']}";
-        $certificate .= str_repeat(' ', $len - strlen($certificate));
-
+        $certificate = "Certificate: " . ($info['has_custom_certificate'] ? 'Yes' : 'No');
         $max_connection = "Max Connections: {$info['max_connections']}";
-        $max_connection .= str_repeat(' ', $len - strlen($max_connection));
+
+        $maxLength = max(strlen($url), strlen($pending_update), strlen($ip_address), strlen($certificate), strlen($max_connection));
+
+        $url = str_pad($url, $maxLength);
+        $pending_update = str_pad($pending_update, $maxLength);
+        $ip_address = str_pad($ip_address, $maxLength);
+        $certificate = str_pad($certificate, $maxLength);
+        $max_connection = str_pad($max_connection, $maxLength);
 
         Console::output()->message($url);
         Console::output()->message($pending_update);
         Console::output()->message($ip_address);
         Console::output()->message($certificate);
-        Console::output()->message($max_connection);
+        Console::output()->message($max_connection, true);
     }
 }
