90565d9cda4ef3f7d6268e390937a2839efe02ed
AmirHossein <amirh.kargar85@gmail.com>
2025-01-21 17:19:34 +0330

    feat: add event cache and clear commands

--- src/Foundation/CoreCommand.php
+++ src/Foundation/CoreCommand.php
@@ -2,6 +2,8 @@
 
 namespace LaraGram\Foundation;
 
+use LaraGram\Console\Console;
+
 class CoreCommand
 {
     private array $commands = [];
@@ -32,35 +34,37 @@ class CoreCommand
     private function baseCommands(): array
     {
         return [
-            Console\GenerateCommand::class,
-            Console\GenerateFactory::class,
-            Console\GenerateMigration::class,
-            Console\GenerateModel::class,
-            Console\GenerateSeeder::class,
-            Console\MigrateCommand::class,
-            Console\SeederCommand::class,
-            Console\GenerateJsonMigration::class,
-            Console\GenerateJsonModel::class,
-            Console\JsonMigrateCommand::class,
-            Console\GenerateProvider::class,
-            Console\GenerateResource::class,
-            Console\SetWebhookCommand::class,
-            Console\DeleteWebhookCommand::class,
-            Console\DropWebhookCommand::class,
-            Console\WebhookInfoCommand::class,
-            Console\GenerateFacade::class,
-            Console\GenerateClass::class,
-            Console\GenerateEnum::class,
-            Console\ServeCommand::class,
-            Console\APIServeCommand::class,
-            Console\GenerateController::class,
-            Console\InstallEloquent::class,
-            Console\InstallOpenSwoole::class,
-            Console\UninstallOpenSwoole::class,
-            Console\UninstallEloquent::class,
-            Console\GenerateConversation::class,
-            Console\ConfigCacheCommand::class,
-            Console\ConfigClearCommand::class,
+            \LaraGram\Foundation\Console\GenerateCommand::class,
+            \LaraGram\Foundation\Console\GenerateFactory::class,
+            \LaraGram\Foundation\Console\GenerateMigration::class,
+            \LaraGram\Foundation\Console\GenerateModel::class,
+            \LaraGram\Foundation\Console\GenerateSeeder::class,
+            \LaraGram\Foundation\Console\MigrateCommand::class,
+            \LaraGram\Foundation\Console\SeederCommand::class,
+            \LaraGram\Foundation\Console\GenerateJsonMigration::class,
+            \LaraGram\Foundation\Console\GenerateJsonModel::class,
+            \LaraGram\Foundation\Console\JsonMigrateCommand::class,
+            \LaraGram\Foundation\Console\GenerateProvider::class,
+            \LaraGram\Foundation\Console\GenerateResource::class,
+            \LaraGram\Foundation\Console\SetWebhookCommand::class,
+            \LaraGram\Foundation\Console\DeleteWebhookCommand::class,
+            \LaraGram\Foundation\Console\DropWebhookCommand::class,
+            \LaraGram\Foundation\Console\WebhookInfoCommand::class,
+            \LaraGram\Foundation\Console\GenerateFacade::class,
+            \LaraGram\Foundation\Console\GenerateClass::class,
+            \LaraGram\Foundation\Console\GenerateEnum::class,
+            \LaraGram\Foundation\Console\ServeCommand::class,
+            \LaraGram\Foundation\Console\APIServeCommand::class,
+            \LaraGram\Foundation\Console\GenerateController::class,
+            \LaraGram\Foundation\Console\InstallEloquent::class,
+            \LaraGram\Foundation\Console\InstallOpenSwoole::class,
+            \LaraGram\Foundation\Console\UninstallOpenSwoole::class,
+            \LaraGram\Foundation\Console\UninstallEloquent::class,
+            \LaraGram\Foundation\Console\GenerateConversation::class,
+            \LaraGram\Foundation\Console\ConfigCacheCommand::class,
+            \LaraGram\Foundation\Console\ConfigClearCommand::class,
+            \LaraGram\Foundation\Console\EventCacheCommand::class,
+            \LaraGram\Foundation\Console\EventClearCommand::class,
         ];
     }
 }
