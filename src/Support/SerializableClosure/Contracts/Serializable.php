4ab1548e71471e506e9434780941585ba1f21104
AmirHossein <amirh.kargar85201@gmail.com>
2025-02-12 23:42:11 +0330

    feat: ass Serializable closure

---
+++ src/Support/SerializableClosure/Contracts/Serializable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LaraGram\Support\SerializableClosure\Contracts;
+
+interface Serializable
+{
+    /**
+     * Resolve the closure with the given arguments.
+     *
+     * @return mixed
+     */
+    public function __invoke();
+
+    /**
+     * Gets the closure that got serialized/unserialized.
+     *
+     * @return \Closure
+     */
+    public function getClosure();
+}
