948a0d0fe64eff8c9e9d571013bd1d21db0c2d6f
AmirHossein <amirh.kargar85201@gmail.com>
2025-02-16 20:13:56 +0330

    feat: add Encryption service

---
+++ src/Encryption/EncryptionServiceProvider.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace LaraGram\Encryption;
+
+use LaraGram\Support\ServiceProvider;
+use LaraGram\Support\Str;
+use LaraGram\Support\SerializableClosure\SerializableClosure;
+
+class EncryptionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerEncrypter();
+        $this->registerSerializableClosureSecurityKey();
+    }
+
+    /**
+     * Register the encrypter.
+     *
+     * @return void
+     */
+    protected function registerEncrypter()
+    {
+        $this->app->singleton('encrypter', function ($app) {
+            $config = $app->make('config')->get('app');
+
+            return (new Encrypter($this->parseKey($config), $config['cipher']))
+                ->previousKeys(array_map(
+                    fn ($key) => $this->parseKey(['key' => $key]),
+                    $config['previous_keys'] ?? []
+                ));
+        });
+    }
+
+    /**
+     * Configure Serializable Closure signing for security.
+     *
+     * @return void
+     */
+    protected function registerSerializableClosureSecurityKey()
+    {
+        $config = $this->app->make('config')->get('app');
+
+        if (! class_exists(SerializableClosure::class) || empty($config['key'])) {
+            return;
+        }
+
+        SerializableClosure::setSecretKey($this->parseKey($config));
+    }
+
+    /**
+     * Parse the encryption key.
+     *
+     * @param  array  $config
+     * @return string
+     */
+    protected function parseKey(array $config)
+    {
+        if (Str::startsWith($key = $this->key($config), $prefix = 'base64:')) {
+            $key = base64_decode(Str::after($key, $prefix));
+        }
+
+        return $key;
+    }
+
+    /**
+     * Extract the encryption key from the given configuration.
+     *
+     * @param  array  $config
+     * @return \LaraGram\Support\HigherOrderTapProxy
+     *
+     * @throws \LaraGram\Encryption\MissingAppKeyException
+     */
+    protected function key(array $config)
+    {
+        return tap($config['key'], function ($key) {
+            if (empty($key)) {
+                throw new MissingAppKeyException;
+            }
+        });
+    }
+}
