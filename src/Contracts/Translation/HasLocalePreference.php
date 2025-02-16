650372fff8460ce9d83fdd52ab73bea189995189
AmirHossein <amirh.kargar85201@gmail.com>
2025-02-16 20:16:57 +0330

    feat: add translation contracts

---
+++ src/Contracts/Translation/HasLocalePreference.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LaraGram\Contracts\Translation;
+
+interface HasLocalePreference
+{
+    /**
+     * Get the preferred locale of the entity.
+     *
+     * @return string|null
+     */
+    public function preferredLocale();
+}
