a4f14f81ed8d1b4edf7b8eb233bc833511785b52
AmirHossein <amirh.kargar85201@gmail.com>
2025-02-16 20:11:15 +0330

    feat: add database queries

---
+++ src/Database/Query/JoinLateralClause.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LaraGram\Database\Query;
+
+class JoinLateralClause extends JoinClause
+{
+    //
+}
---
+++ src/Database/Query/Processors/MariaDbProcessor.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LaraGram\Database\Query\Processors;
+
+class MariaDbProcessor extends MySqlProcessor
+{
+    //
+}
