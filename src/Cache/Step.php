a790e28b7a4a59d662df5d4cb404a0ea17a427a3
AmirHossein <amirh.kargar85@gmail.com>
2025-07-04 16:13:39 +0330

    feat: add step managemer

---
+++ src/Cache/Step.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace LaraGram\Cache;
+
+use LaraGram\Support\Facades\Cache;
+use LaraGram\Support\Facades\Hash;
+
+class Step
+{
+    /**
+     * The cache store implementation.
+     *
+     * @var \LaraGram\Contracts\Cache\Repository
+     */
+    protected $cache;
+
+    /**
+     * The step key name
+     *
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * Create a new step manager instance.
+     *
+     * @param  \LaraGram\Contracts\Cache\Repository  $cache
+     * @return void
+     */
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+        $this->key = Hash::make(id().":step");
+    }
+
+    /**
+     * Create and set new step.
+     *
+     * @param  string $step
+     * @param  int|null $ttl
+     * @return bool
+     */
+    public function set(string $step, int $ttl = null): bool
+    {
+        return $this->cache->set($this->key, $step, $ttl);
+    }
+
+    /**
+     * Get current step.
+     *
+     * @return mixed
+     */
+    public function get(): mixed
+    {
+        return $this->cache->get($this->key);
+    }
+
+    /**
+     * Clear current step.
+     *
+     * @return bool
+     */
+    public function forget(): bool
+    {
+        return $this->cache->forget($this->key);
+    }
+
+    /**
+     * Check user has any step.
+     *
+     * @return bool
+     */
+    public function hasStep(): bool
+    {
+        return $this->cache->has($this->key);
+    }
+
+    /**
+     * Check user has not any step.
+     *
+     * @return bool
+     */
+    public function hasNotStep(): bool
+    {
+        return ! $this->hasStep();
+    }
+
+    /**
+     * Get and clear current step.
+     *
+     * @return mixed
+     */
+    public function pull(): mixed
+    {
+        return $this->cache->pull($this->key);
+    }
+}
