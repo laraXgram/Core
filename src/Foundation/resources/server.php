a773f867233322e1b07cd955a32ba369a1713053
AmirHossein <amirh.kargar85@gmail.com>
2025-07-18 01:44:30 +0330

    fix: missing server.php

---
+++ src/Foundation/resources/server.php
@@ -0,0 +1,15 @@
+<?php
+
+$publicPath = getcwd();
+
+if (!file_exists($index = $publicPath.'/index.php')) {
+    $index = $publicPath.'/public/index.php';
+}
+
+$server = escapeshellarg(json_encode($_SERVER));
+$inputs = escapeshellarg(file_get_contents('php://input'));
+
+$output = '/dev/null'; // You can change it to specifics file.
+$output = 'log.log'; // You can change it to specifics file.
+
+popen("php {$index} {$inputs} {$server} >> {$output} 2>&1 &", "r");
