2b4007284174b81c83f125c14c35c8d67fa43372
AmirHossein <amirh.kargar85@gmail.com>
2025-07-17 23:19:11 +0330

    fix: some service provider docs

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -7,42 +7,114 @@ use LaraGram\Contracts\Foundation\CachesConfiguration;
 use LaraGram\Contracts\Foundation\CachesListens;
 use LaraGram\Contracts\Support\DeferrableProvider;
 use LaraGram\Console\Application as Commander;
+use LaraGram\Template\Compilers\Temple8Compiler;
 
 abstract class ServiceProvider
 {
     /**
-     * @var \LaraGram\Contracts\Foundation\Application $app
+     * The application instance.
+     *
+     * @var \LaraGram\Contracts\Foundation\Application
      */
     protected $app;
-    protected array $bootingCallbacks = [];
-    protected array $bootedCallbacks = [];
-    public static array $publishes = [];
-    public static array $publishGroups = [];
-    protected static array $publishableMigrationPaths = [];
+
+    /**
+     * All of the registered booting callbacks.
+     *
+     * @var array
+     */
+    protected $bootingCallbacks = [];
+
+    /**
+     * All of the registered booted callbacks.
+     *
+     * @var array
+     */
+    protected $bootedCallbacks = [];
+
+    /**
+     * The paths that should be published.
+     *
+     * @var array
+     */
+    public static $publishes = [];
+
+    /**
+     * The paths that should be published by group.
+     *
+     * @var array
+     */
+    public static $publishGroups = [];
+
+    /**
+     * The migration paths available for publishing.
+     *
+     * @var array
+     */
+    protected static $publishableMigrationPaths = [];
+
+    /**
+     * Commands that should be run during the "optimize" command.
+     *
+     * @var array<string, string>
+     */
     public static array $optimizeCommands = [];
+
+    /**
+     * Commands that should be run during the "optimize:clear" command.
+     *
+     * @var array<string, string>
+     */
     public static array $optimizeClearCommands = [];
 
+    /**
+     * Create a new service provider instance.
+     *
+     * @param  \LaraGram\Contracts\Foundation\Application  $app
+     */
     public function __construct($app)
     {
         $this->app = $app;
     }
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
         //
     }
 
-    public function booting(Closure $callback): void
+    /**
+     * Register a booting callback to be run before the "boot" method is called.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function booting(Closure $callback)
     {
         $this->bootingCallbacks[] = $callback;
     }
 
-    public function booted(Closure $callback): void
+    /**
+     * Register a booted callback to be run after the "boot" method is called.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function booted(Closure $callback)
     {
         $this->bootedCallbacks[] = $callback;
     }
 
-    public function callBootingCallbacks(): void
+    /**
+     * Call the registered booting callbacks.
+     *
+     * @return void
+     */
+    public function callBootingCallbacks()
     {
         $index = 0;
 
@@ -53,7 +125,12 @@ abstract class ServiceProvider
         }
     }
 
-    public function callBootedCallbacks(): void
+    /**
+     * Call the registered booted callbacks.
+     *
+     * @return void
+     */
+    public function callBootedCallbacks()
     {
         $index = 0;
 
@@ -64,6 +141,13 @@ abstract class ServiceProvider
         }
     }
 
+    /**
+     * Merge the given configuration with the existing configuration.
+     *
+     * @param  string  $path
+     * @param  string  $key
+     * @return void
+     */
     protected function mergeConfigFrom($path, $key)
     {
         if (! ($this->app instanceof CachesConfiguration && $this->app->configurationIsCached())) {
@@ -75,9 +159,16 @@ abstract class ServiceProvider
         }
     }
 
+    /**
+     * Replace the given configuration with the existing configuration recursively.
+     *
+     * @param  string  $path
+     * @param  string  $key
+     * @return void
+     */
     protected function replaceConfigRecursivelyFrom($path, $key)
     {
-        if (!($this->app instanceof CachesConfiguration && $this->app->configurationIsCached())) {
+        if (! ($this->app instanceof CachesConfiguration && $this->app->configurationIsCached())) {
             $config = $this->app->make('config');
 
             $config->set($key, array_replace_recursive(
@@ -99,13 +190,65 @@ abstract class ServiceProvider
         }
     }
 
-    protected function loadTranslationsFrom($path, $namespace)
+    /**
+     * Register a template file namespace.
+     *
+     * @param  string|array  $path
+     * @param  string  $namespace
+     * @return void
+     */
+    protected function loadTemplatesFrom($path, $namespace)
     {
-        $this->callAfterResolving('translator', function ($translator) use ($path, $namespace) {
-            $translator->addNamespace($namespace, $path);
+        $this->callAfterResolving('template', function ($template) use ($path, $namespace) {
+            if (isset($this->app->config['template']['paths']) &&
+                is_array($this->app->config['template']['paths'])) {
+                foreach ($this->app->config['template']['paths'] as $templatePath) {
+                    if (is_dir($appPath = $templatePath.'/vendor/'.$namespace)) {
+                        $template->addNamespace($namespace, $appPath);
+                    }
+                }
+            }
+
+            $template->addNamespace($namespace, $path);
         });
     }
 
+    /**
+     * Register the given template components with a custom prefix.
+     *
+     * @param  string  $prefix
+     * @param  array  $components
+     * @return void
+     */
+    protected function loadTemplateComponentsAs($prefix, array $components)
+    {
+        $this->callAfterResolving(Temple8Compiler::class, function ($temple) use ($prefix, $components) {
+            foreach ($components as $alias => $component) {
+                $temple->component($component, is_string($alias) ? $alias : null, $prefix);
+            }
+        });
+    }
+
+    /**
+     * Register a translation file namespace or path.
+     *
+     * @param  string  $path
+     * @param  string|null  $namespace
+     * @return void
+     */
+    protected function loadTranslationsFrom($path, $namespace = null)
+    {
+        $this->callAfterResolving('translator', fn ($translator) => is_null($namespace)
+            ? $translator->addPath($path)
+            : $translator->addNamespace($namespace, $path));
+    }
+
+    /**
+     * Register a JSON translation file path.
+     *
+     * @param  string  $path
+     * @return void
+     */
     protected function loadJsonTranslationsFrom($path)
     {
         $this->callAfterResolving('translator', function ($translator) use ($path) {
@@ -113,6 +256,12 @@ abstract class ServiceProvider
         });
     }
 
+    /**
+     * Register database migration paths.
+     *
+     * @param  array|string  $paths
+     * @return void
+     */
     protected function loadMigrationsFrom($paths)
     {
         $this->callAfterResolving('migrator', function ($migrator) use ($paths) {
@@ -122,6 +271,14 @@ abstract class ServiceProvider
         });
     }
 
+    /**
+     * Register Eloquent model factory paths.
+     *
+     * @deprecated Will be removed in a future Laravel version.
+     *
+     * @param  array|string  $paths
+     * @return void
+     */
     protected function loadFactoriesFrom($paths)
     {
         $this->callAfterResolving(ModelFactory::class, function ($factory) use ($paths) {
@@ -131,8 +288,14 @@ abstract class ServiceProvider
         });
     }
 
-
-    protected function callAfterResolving($name, $callback): void
+    /**
+     * Setup an after resolving listener, or fire immediately if already resolved.
+     *
+     * @param  string  $name
+     * @param  callable  $callback
+     * @return void
+     */
+    protected function callAfterResolving($name, $callback)
     {
         $this->app->afterResolving($name, $callback);
 
@@ -141,6 +304,13 @@ abstract class ServiceProvider
         }
     }
 
+    /**
+     * Register migration paths to be published by the publish command.
+     *
+     * @param  array  $paths
+     * @param  mixed  $groups
+     * @return void
+     */
     protected function publishesMigrations(array $paths, $groups = null)
     {
         $this->publishes($paths, $groups);
@@ -150,27 +320,47 @@ abstract class ServiceProvider
         }
     }
 
-    protected function publishes(array $paths, $groups = null): void
+    /**
+     * Register paths to be published by the publish command.
+     *
+     * @param  array  $paths
+     * @param  mixed  $groups
+     * @return void
+     */
+    protected function publishes(array $paths, $groups = null)
     {
         $this->ensurePublishArrayInitialized($class = static::class);
 
         static::$publishes[$class] = array_merge(static::$publishes[$class], $paths);
 
-        foreach ((array)$groups as $group) {
+        foreach ((array) $groups as $group) {
             $this->addPublishGroup($group, $paths);
         }
     }
 
-    protected function ensurePublishArrayInitialized($class): void
+    /**
+     * Ensure the publish array for the service provider is initialized.
+     *
+     * @param  string  $class
+     * @return void
+     */
+    protected function ensurePublishArrayInitialized($class)
     {
-        if (!array_key_exists($class, static::$publishes)) {
+        if (! array_key_exists($class, static::$publishes)) {
             static::$publishes[$class] = [];
         }
     }
 
-    protected function addPublishGroup($group, $paths): void
+    /**
+     * Add a publish group / tag to the service provider.
+     *
+     * @param  string  $group
+     * @param  array  $paths
+     * @return void
+     */
+    protected function addPublishGroup($group, $paths)
     {
-        if (!array_key_exists($group, static::$publishGroups)) {
+        if (! array_key_exists($group, static::$publishGroups)) {
             static::$publishGroups[$group] = [];
         }
 
@@ -179,6 +369,13 @@ abstract class ServiceProvider
         );
     }
 
+    /**
+     * Get the paths to publish.
+     *
+     * @param  string|null  $provider
+     * @param  string|null  $group
+     * @return array
+     */
     public static function pathsToPublish($provider = null, $group = null)
     {
         if (! is_null($paths = static::pathsForProviderOrGroup($provider, $group))) {
@@ -190,6 +387,13 @@ abstract class ServiceProvider
         }, []);
     }
 
+    /**
+     * Get the paths for the provider or group (or both).
+     *
+     * @param  string|null  $provider
+     * @param  string|null  $group
+     * @return array
+     */
     protected static function pathsForProviderOrGroup($provider, $group)
     {
         if ($provider && $group) {
@@ -203,31 +407,59 @@ abstract class ServiceProvider
         }
     }
 
-    protected static function pathsForProviderAndGroup($provider, $group): array
+    /**
+     * Get the paths for the provider and group.
+     *
+     * @param  string  $provider
+     * @param  string  $group
+     * @return array
+     */
+    protected static function pathsForProviderAndGroup($provider, $group)
     {
-        if (!empty(static::$publishes[$provider]) && !empty(static::$publishGroups[$group])) {
+        if (! empty(static::$publishes[$provider]) && ! empty(static::$publishGroups[$group])) {
             return array_intersect_key(static::$publishes[$provider], static::$publishGroups[$group]);
         }
 
         return [];
     }
 
-    public static function publishableProviders(): array
+    /**
+     * Get the service providers available for publishing.
+     *
+     * @return array
+     */
+    public static function publishableProviders()
     {
         return array_keys(static::$publishes);
     }
 
+    /**
+     * Get the migration paths available for publishing.
+     *
+     * @return array
+     */
     public static function publishableMigrationPaths()
     {
         return static::$publishableMigrationPaths;
     }
 
-    public static function publishableGroups(): array
+    /**
+     * Get the groups available for publishing.
+     *
+     * @return array
+     */
+    public static function publishableGroups()
     {
         return array_keys(static::$publishGroups);
     }
 
-    public function commands($commands): void
+    /**
+     * Register the package's custom Commander commands.
+     *
+     * @param  array|mixed  $commands
+     * @return void
+     */
+    public function commands($commands)
     {
         $commands = is_array($commands) ? $commands : func_get_args();
 
@@ -236,6 +468,14 @@ abstract class ServiceProvider
         });
     }
 
+    /**
+     * Register commands that should run on "optimize" or "optimize:clear".
+     *
+     * @param  string|null  $optimize
+     * @param  string|null  $clear
+     * @param  string|null  $key
+     * @return void
+     */
     protected function optimizes(?string $optimize = null, ?string $clear = null, ?string $key = null)
     {
         $key ??= (string) Str::of(get_class($this))
@@ -258,26 +498,53 @@ abstract class ServiceProvider
         }
     }
 
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
     public function provides()
     {
         return [];
     }
 
-    public function when(): array
+    /**
+     * Get the events that trigger this service provider to register.
+     *
+     * @return array
+     */
+    public function when()
     {
         return [];
     }
 
-    public function isDeferred(): bool
+    /**
+     * Determine if the provider is deferred.
+     *
+     * @return bool
+     */
+    public function isDeferred()
     {
         return $this instanceof DeferrableProvider;
     }
 
+    /**
+     * Get the default providers for a Laravel application.
+     *
+     * @return \LaraGram\Support\DefaultProviders
+     */
     public static function defaultProviders()
     {
         return new DefaultProviders;
     }
 
+    /**
+     * Add the given provider to the application's provider bootstrap file.
+     *
+     * @param  string  $provider
+     * @param  string  $path
+     * @return bool
+     */
     public static function addProviderToBootstrapFile(string $provider, ?string $path = null)
     {
         $path ??= app()->getBootstrapProvidersPath();
