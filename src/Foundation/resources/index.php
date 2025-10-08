7ff0cc8943f406093aaa6d4d01e0ddd953bb8fbc
AmirHkrg <amirh.kargar85@gmail.com>
2025-10-08 22:46:09 +0330

    refactor: resources index file

--- src/Foundation/resources/index.php
+++ src/Foundation/resources/index.php
@@ -5,7 +5,4 @@ $inputs = escapeshellarg(file_get_contents('php://input'));
 
 $serverPath = __DIR__."/server.php";
 
-$output = '/dev/null'; // You can change it to specifics file.
-$output = 'log.log'; // You can change it to specifics file.
-
-popen("php \"{$serverPath}\" {$inputs} {$server} >> {$output} 2>&1 &", "r");
+popen("php \"{$serverPath}\" {$inputs} {$server} >> /dev/null 2>&1 &", "r");
