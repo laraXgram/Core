89cfe59699f70e4f99e01b029e82e7e67c274ed6
amirhossein <amirh.kargar85@gmail.com>
2026-06-03 17:11:21 +0330

    chore: add `HandleMultiBotUpdate` middleware for handle multi bot with one code base

---
+++ src/Foundation/Bot/Middleware/HandleMultiBotUpdate.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaraGram\Foundation\Bot\Middleware;
+
+use Closure;
+use LaraGram\Request\Request;
+
+class HandleMultiBotUpdate
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \LaraGram\Request\Request  $request
+     * @param  \Closure  $next
+     * @return \LaraGram\Request\Response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (config('bot.default') === 'auto') {
+            foreach (config('bot.connections') as $name => $config) {
+                if (($config['secret_token'] ?? null) === $request->secretToken()) {
+                    Request::setDefaultConnection($name);
+                    return $next($request);
+                }
+            }
+        }
+    }
+}
