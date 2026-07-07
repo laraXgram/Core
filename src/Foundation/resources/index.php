37ace685b3808f30888e36118a22bd9519b71bef
amirhossein <amirh.kargar85@gmail.com>
2026-07-08 01:14:15 +0330

    fix: server index

--- src/Foundation/resources/index.php
+++ src/Foundation/resources/index.php
@@ -5,16 +5,8 @@ $serverPath = __DIR__."/server.php";
 if (($content = json_decode(file_get_contents('php://input'), true)) == null) {
     require_once $serverPath;
 } else {
-    $request = escapeshellarg(json_encode([
-        'TIME' => time(),
-        'HASH' => md5(uniqid(rand(), true)),
-        '_' => [
-            '_GET' => $_GET,
-            '_POST' => $_POST,
-            '_SERVER' => $_SERVER,
-            '_CONTENTS' => $content,
-        ]
-    ]));
+    $server = escapeshellarg(json_encode($_SERVER));
+    $inputs = escapeshellarg(file_get_contents('php://input'));
 
-    popen("php \"{$serverPath}\" {$request} >> /dev/null 2>&1 &", "r");
+    popen("php \"{$serverPath}\" {$inputs} {$server} >> /dev/null 2>&1 &", "r");
 }
