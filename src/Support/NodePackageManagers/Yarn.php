39b2ab0c214d285dcf09720f62e4bd0dbe05d7bf
amirhossein <amirh.kargar85@gmail.com>
2026-07-08 00:47:51 +0330

    feat: add NodePackageManager

---
+++ src/Support/NodePackageManagers/Yarn.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LaraGram\Support\NodePackageManagers;
+
+use LaraGram\Support\Contracts\NodePackageManager;
+
+class Yarn implements NodePackageManager
+{
+    /**
+     * Determine if the Yarn package manager is in use.
+     *
+     * @return bool
+     */
+    public static function matches(): bool
+    {
+        return file_exists(getcwd().'/yarn.lock');
+    }
+
+    /**
+     * Get the command to run a script using Yarn.
+     *
+     * @param  string  $command
+     * @return string
+     */
+    public function getRunCommand(string $command): string
+    {
+        return "yarn run {$command}";
+    }
+
+    /**
+     * Get the command to execute a package using Yarn.
+     *
+     * @param  string  $command
+     * @return string
+     */
+    public function getExecCommand(string $command): string
+    {
+        return "yarn dlx {$command}";
+    }
+}
