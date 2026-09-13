a3f3cf44bd12a7b42c482b39edfff4f3dd8d2fcc
AmirHossein <amirh.kargar85@gmail.com>
2026-09-13 23:08:44 +0330

    fix: pagination state request split

--- src/Pagination/PaginationState.php
+++ src/Pagination/PaginationState.php
@@ -14,10 +14,10 @@ class PaginationState
     {
         Paginator::viewFactoryResolver(fn () => $app['view']);
 
-        Paginator::currentPathResolver(fn () => $app['request']->url());
+        Paginator::currentPathResolver(fn () => $app['http.request']->url());
 
         Paginator::currentPageResolver(function ($pageName = 'page') use ($app) {
-            $page = $app['request']->input($pageName);
+            $page = $app['http.request']->input($pageName);
 
             if (filter_var($page, FILTER_VALIDATE_INT) !== false && (int) $page >= 1) {
                 return (int) $page;
@@ -26,10 +26,10 @@ class PaginationState
             return 1;
         });
 
-        Paginator::queryStringResolver(fn () => $app['request']->query());
+        Paginator::queryStringResolver(fn () => $app['http.request']->query());
 
         CursorPaginator::currentCursorResolver(function ($cursorName = 'cursor') use ($app) {
-            return Cursor::fromEncoded($app['request']->input($cursorName));
+            return Cursor::fromEncoded($app['http.request']->input($cursorName));
         });
 
         static::resolveTelegramUsing($app);
